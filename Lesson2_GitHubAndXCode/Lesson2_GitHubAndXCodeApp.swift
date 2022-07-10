0d5547d9a59ccfff3a11bb9ce8e028cf24f19888
Abhay <aragarwal23@icloud.com>
2022-07-10 12:49:29 -0500

    Initial Commit

---
+++ Lesson2_GitHubAndXCode/Lesson2_GitHubAndXCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson2_GitHubAndXCodeApp.swift
+//  Lesson2_GitHubAndXCode
+//
+//  Created by Subh Agarwal on 7/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Lesson2_GitHubAndXCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
